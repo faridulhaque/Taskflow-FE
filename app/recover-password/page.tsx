9f5075d38337124161d9b3b65dfbd002ac7fe01e
Faridul Haque Murshed <faridmurshed9@gmail.com>
2025-11-06 08:54:01 +0600

    suspense added for recovery page

--- app/recover-password/page.tsx
+++ app/recover-password/page.tsx
@@ -1,12 +1,15 @@
+import Loading from "@/components/Loading";
 import Navbar from "@/components/Navbar";
 import RecoverPassword from "@/components/RecoverPassword";
-import React from "react";
+import React, { Suspense } from "react";
 
 export default function page() {
   return (
     <div className="min-h-screen bg-[#0F172A]">
-      <Navbar />
-      <RecoverPassword></RecoverPassword>
+      <Suspense fallback={<Loading></Loading>}>
+        <Navbar />
+        <RecoverPassword></RecoverPassword>
+      </Suspense>
     </div>
   );
 }
